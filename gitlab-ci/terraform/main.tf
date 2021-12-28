4ecac5b98b9f3b63c6a43ffda5e066bc0e0830ee
Armand Kulsh <zenars85@mail.ru>
2021-12-28 13:32:28 +0300

    Add terraform files

---
+++ gitlab-ci/terraform/main.tf
@@ -0,0 +1,31 @@
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
+
+resource "yandex_compute_instance" "gitblab-ci" {
+  name  = "gitblab-ci-vm"
+  labels = {
+    tags = "gitblab-ci"
+  }
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+      size = var.disk_size
+    }
+  }
+  network_interface {
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+}
