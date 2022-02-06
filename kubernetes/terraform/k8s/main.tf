97ee0eabb4a5c9ad29ed2011b88848c27ce51a27
Armand Kulsh <zenars85@mail.ru>
2022-02-06 11:51:31 +0300

    Kubernetes-2

---
+++ kubernetes/terraform/k8s/main.tf
@@ -0,0 +1,54 @@
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
+
+resource "yandex_kubernetes_cluster" "cluster" {
+  name                    = "reddit"
+  folder_id               = var.folder_id
+  network_id              = var.network_id
+  service_account_id      = var.service_account_id
+  node_service_account_id = var.service_account_id
+  release_channel         = "RAPID"
+
+  master {
+    version   = "1.19"
+    public_ip = true
+
+    zonal {
+      zone      = var.zone
+      subnet_id = var.subnet_id
+    }
+  }
+}
+
+resource "yandex_kubernetes_node_group" "node_groups" {
+  cluster_id  = yandex_kubernetes_cluster.cluster.id
+  name        = "reddit-group"
+  version     = "1.19"
+
+  instance_template {
+    platform_id = "standard-v2"
+    nat         = true
+    metadata = {
+      ssh-keys = "ubuntu:${file(var.public_key_path)}"
+
+    }
+
+    resources {
+      cores  = 4
+      memory = 8
+    }
+    boot_disk {
+      size = 64
+    }
+
+  }
+  scale_policy {
+    fixed_scale {
+      size = 2
+    }
+  }
+}
